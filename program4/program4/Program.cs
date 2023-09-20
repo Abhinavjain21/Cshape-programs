5421a36a3572f418ee1d442c0e19dce6634fd940
Abhinavjain21 <129595224+Abhinavjain21@users.noreply.github.com>
2023-09-21 01:04:01 +0530

    Add files via upload

---
+++ program4/program4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String to remove all the whitespaces from it");
+            String str=Console.ReadLine();
+            String str1=String.Empty;
+            for(int i=0;i<str.Length;i++)
+            {
+                if (str[i]==' ')
+                {
+
+                }
+                else
+                {
+                    str1=str1+ str[i];
+                }
+            }
+            Console.WriteLine(str1);
+            Console.ReadLine();
+
+        }
+    }
+}
