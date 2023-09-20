124add119ba32cca9d0dba1a1e4f69971914d7db
Abhinavjain21 <129595224+Abhinavjain21@users.noreply.github.com>
2023-09-20 21:18:31 +0530

    Add files via upload

---
+++ program1/palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String");
+            String name=Console.ReadLine();
+            String rev =String.Empty;
+            char[] chr= name.ToCharArray();
+            for(int i=chr.Length-1;i>=0;i--)
+            {
+                rev = rev + chr[i];
+            }
+            if(rev==name)
+            {
+                Console.WriteLine("String is Palindrome {0}", name);
+            }
+            else
+            {
+                Console.WriteLine("String is not Palindrome");
+            }
+          
+            Console.ReadLine();
+        }
+    }
+}
---
+++ program2/count/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String");
+            String str=Console.ReadLine();
+           
+            int cv = 0,cc=0,sc=0;
+            foreach (char c in str)
+            {
+                if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                {
+                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    {
+                        cv++;
+                    }
+                    else if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                    {
+                        cv++;
+                    }
+                    else
+                    {
+                        cc++;
+                    }
+
+                }
+               
+            }
+                for(int i = 0; i < str.Length; i++)
+                {
+                    if (str[i]== ' ')
+                    {
+                        sc++;
+                    }
+                }
+            Console.WriteLine("Total number of vowels,spaces and consonents are: {0} {1} {2}", cv, sc,cc);
+            Console.ReadLine();
+        }
+    }
+}
