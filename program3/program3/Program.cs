5e1e6900be071c63c7f861e58d756464fcaf9bc0
Abhinavjain21 <129595224+Abhinavjain21@users.noreply.github.com>
2023-09-21 00:59:53 +0530

    Add files via upload

---
+++ program3/program3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String");
+            String str = Console.ReadLine();
+            String str1 = String.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'||
+                    str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+            {
+
+
+            }
+            else
+            {
+                str1 = str1 + str[i];
+
+            }
+        }
+            Console.WriteLine(str1);
+            Console.ReadLine();
+        }
+    }
+}
