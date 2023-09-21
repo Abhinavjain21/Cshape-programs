8914a77695b847d8f0ccadb499f51b8eec152de6
Abhinavjain21 <129595224+Abhinavjain21@users.noreply.github.com>
2023-09-21 19:08:22 +0530

    Add files via upload

---
+++ program7/program7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String");
+            String str=Console.ReadLine();
+            String str1 =String.Empty;
+            str=str.ToLower();
+            char[] ch=str.ToCharArray();
+             for(int i = 0; i < ch.Length; i++)
+            {
+                
+                if (i == 0 || i == ch.Length - 1)
+                {
+                    ch[i] = char.ToUpper(ch[i]);
+                }
+                else if (ch[i] ==' ')
+                {
+                    if (i - 1 >= 0)
+                    {
+                        ch[i - 1] = char.ToUpper(ch[i - 1]);
+                    }
+                    if (i + 1 >= 0)
+                    {
+                        ch[i + 1] = char.ToUpper(ch[i + 1]);
+                    }
+                }
+                
+            }
+            String result = new string(ch);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
