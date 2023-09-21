7cb32da2812a7369c60014e7fe4ca123a05d6fb2
Abhinavjain21 <129595224+Abhinavjain21@users.noreply.github.com>
2023-09-21 19:58:26 +0530

    Add files via upload

---
+++ program9/program9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String To Find The Largest word:");
+            String str=Console.ReadLine();
+            String[] words = str.Split(new[] {" "},StringSplitOptions.None);
+            String word = " ";
+            int n = 0;
+            foreach(String s in words)
+            {
+                if(s.Length>n)
+                {
+                    word = s;
+                    n = s.Length;
+                }
+            }
+            Console.WriteLine(word);
+            Console.ReadLine();
+        }
+    }
+}
