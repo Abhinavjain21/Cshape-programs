c8c4de2e91c1a1d7a2574acff75d3774dd298076
Abhinavjain21 <129595224+Abhinavjain21@users.noreply.github.com>
2023-09-21 20:02:42 +0530

    Add files via upload

---
+++ program6/program6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static int SumNumbersInString(string input)
+    {
+        int sum = 0;
+        StringBuilder currentNumber = new StringBuilder();
+
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+            {
+                currentNumber.Append(c);
+            }
+            else if (currentNumber.Length > 0)
+            {
+                sum += int.Parse(currentNumber.ToString());
+                currentNumber.Clear();
+            }
+        }
+
+        if (currentNumber.Length > 0)
+        {
+            sum += int.Parse(currentNumber.ToString());
+        }
+
+        return sum;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("enter a String");
+        string input = Console.ReadLine();
+
+        int sum = SumNumbersInString(input);
+
+        Console.WriteLine("Sum of numbers in the string: " + sum);
+        Console.ReadLine();
+    }
+}
